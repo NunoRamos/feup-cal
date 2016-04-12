e3da0ee72db9921011dcb1ef4cacf0ceae5816ef
Edgar Passos <epassos26@gmail.com>
2016-04-12 15:38:32 +0100

    Created project and added class header files

---
+++ proj_part1/src/Passenger.h
@@ -0,0 +1,24 @@
+/*
+ * Passenger.h
+ *
+ *  Created on: Apr 12, 2016
+ *     Authors: Daniel Garrido, Edgar Passos, Nuno Ramos
+ */
+
+#ifndef PASSENGER_H_
+#define PASSENGER_H_
+
+using namespace std;
+
+class Passenger{
+
+	string name;
+	unsigned int nif;
+	//vertex destino
+	Reservation reservation;
+
+};
+
+
+
+#endif /* PASSENGER_H_ */
---
+++ proj_part1/src/Reservation.h
@@ -0,0 +1,22 @@
+/*
+ * Reservation.h
+ *
+ *  Created on: Apr 12, 2016
+ *     Authors: Daniel Garrido, Edgar Passos, Nuno Ramos
+ */
+
+#ifndef RESERVATION_H_
+#define RESERVATION_H_
+
+
+using namespace std;
+
+class Reservation{
+
+	//TODO vertex destination
+	string arrivalTime;
+};
+
+
+
+#endif /* RESERVATION_H_ */
