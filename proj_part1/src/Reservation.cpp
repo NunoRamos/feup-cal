61de1fdad6167ef947406a7c00441f5c1105221b
dalugoga <dalugoga@gmail.com>
2016-04-25 17:08:20 +0100

    Overloaded < operator of Reservation

--- proj_part1/src/Reservation.cpp
+++ proj_part1/src/Reservation.cpp
@@ -6,15 +6,36 @@ Reservation::Reservation(Node *destination, string arrivalTime, Passenger *clien
 	this->client = client;
 }
 
-Node *Reservation::getDestination(){
+Node *Reservation::getDestination() const{
 	return destination;
 }
 
-string Reservation::getArrivalTime(){
+string Reservation::getArrivalTime() const{
 	return arrivalTime;
 }
 
-Passenger* Reservation::getClient(){
+Passenger* Reservation::getClient() const{
 	return client;
 }
+
+
+bool Reservation::operator <(const Reservation &res1) const
+{
+	stringstream ss;
+	int r1, r2;
+
+	ss << arrivalTime;
+	ss >> r1;
+
+	ss.clear();
+
+	ss << res1.getArrivalTime();
+	ss >> r2;
+
+	if(r1 > r2)
+		return true;
+	else
+		return false;
+}
+
 
